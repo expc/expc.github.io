434f5ab916ae701982550c5b2a39ad310c26d897
i76JxydJ0xF <i76JxydJ0xF@qCtM.st>
2019-10-07 10:42:50 +0800

    2019年10月 7日 10:42:50

--- static/eos.js
+++ static/eos.js
@@ -856,7 +856,7 @@ function nodetype3_data(data) {
     }
     // console.log(cur_quan_total);
     // console.log(total_quan);
-    var cur_quan_html='<s style="color: #03ecc1;">'+cur_quan_total +'</s>' + '<s style="color: #fff">/</s>' + (parseInt(total_quan)+parseInt(static_node));
+    var cur_quan_html='<s style="color: #03ecc1;">'+(parseInt(cur_quan_total)+parseInt(static_node)) +'</s>' + '<s style="color: #fff">/</s>' + total_quan;
     $("#cur_quan").html(cur_quan_html);
     $(".nodetype3").html(html);
     scatter_status();
